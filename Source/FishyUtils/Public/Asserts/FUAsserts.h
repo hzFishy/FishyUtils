98203633cf24653adef369c03ab7fdb6ca591ada
hzFishy <84443782+hzFishy@users.noreply.github.com>
2025-04-07 22:47:18 +0200

    added asserts

---
+++ Source/FishyUtils/Public/Asserts/FUAsserts.h
@@ -0,0 +1,17 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+// taken from AssertionMacros.h (see definition of always)
+#if DO_ENSURE && !USING_CODE_ANALYSIS
+
+	#define FU_ASRT_ALWAYS(InExpression)							ensureAlways(InExpression)
+	#define FU_ASRT_ALWAYSMSG(InExpression, InFormat, ... )			ensureAlwaysMsgf(InExpression, TEXT(InFormat), ##__VA_ARGS__)
+
+#else
+
+	#define FU_ASRT_ALWAYS(InExpression)							{}
+	#define FU_ASRT_ALWAYSMSG(InExpression, InFormat, ... )			{}
+
+#endif
