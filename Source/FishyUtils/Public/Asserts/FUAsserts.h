832ea7abcaacb9b534840bbfd99865d351d3588c
hzFishy <84443782+hzFishy@users.noreply.github.com>
2025-08-12 03:18:36 +0200

    feat(asserts): changed assert macro names and added new ones

--- Source/FishyUtils/Public/Asserts/FUAsserts.h
+++ Source/FishyUtils/Public/Asserts/FUAsserts.h
@@ -6,12 +6,15 @@
 // taken from AssertionMacros.h (see definition of always)
 #if DO_ENSURE && !USING_CODE_ANALYSIS
 
-	#define FU_ASRT_ALWAYS(InExpression)							ensureAlways(InExpression)
-	#define FU_ASRT_ALWAYSMSG(InExpression, InFormat, ... )			ensureAlwaysMsgf(InExpression, TEXT(InFormat), ##__VA_ARGS__)
+	#define FU_ALWAYS(InExpression)								ensureAlways(InExpression)
+	#define FU_ALWAYSMSG(InExpression, InFormat, ...)			ensureAlwaysMsgf(InExpression, TEXT(InFormat), ##__VA_ARGS__)
 
 #else
 
-	#define FU_ASRT_ALWAYS(InExpression)							(LIKELY(!!(InExpression)))
-	#define FU_ASRT_ALWAYSMSG(InExpression, InFormat, ... )			(LIKELY(!!(InExpression)))
+	#define FU_ALWAYS(InExpression)								(LIKELY(!!(InExpression)))
+	#define FU_ALWAYSMSG(InExpression, InFormat, ...)			(LIKELY(!!(InExpression)))
 
 #endif
+
+	#define FU_ALWAYSVALID(Object)								FU_ALWAYS(IsValid(Object))
+	#define FU_ALWAYSVALIDMSG(Object, InFormat, ...)			FU_ALWAYSMSG(IsValid(Object), InFormat, ##__VA_ARGS__)
