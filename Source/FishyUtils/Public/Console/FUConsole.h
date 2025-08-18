7d0c68b26a29fb59445bb78b1cdd09d233356c6f
hzFishy <84443782+hzFishy@users.noreply.github.com>
2025-08-18 05:22:38 +0200

    refacto: removed FU_CONSOLE_PROJECTNAME and reformated functions signatures

--- Source/FishyUtils/Public/Console/FUConsole.h
+++ Source/FishyUtils/Public/Console/FUConsole.h
@@ -9,11 +9,6 @@
 	#define FU_WITH_CONSOLE 1
 #endif
 
-#ifndef FU_CONSOLE_PROJECTNAME
-	// edit this in your Build.cs file using PublicDefinitions.Add("FU_CONSOLE_PROJECTNAME=\"PROJECTNAME\"");
-	#define FU_CONSOLE_PROJECTNAME "FU"
-#endif
-
 namespace FU_Console
 {
 
@@ -24,54 +19,38 @@ namespace FU_Console
 	 */
 	static FString BuildFullCommandString(const FString& Category, const FString& CommandName)
 	{
-		return FString::Printf(TEXT("%s.%s.%s"), TEXT(FU_CONSOLE_PROJECTNAME), *Category, *CommandName);
+		return FString::Printf(TEXT("%s.%s"), *Category, *CommandName);
 	};
 
 	/* No world, no args */
 	struct FFUAutoConsoleCommand : private FAutoConsoleCommand
 	{
-		FFUAutoConsoleCommand(const FString& Category, const FString& CommandName,
-			const FString& HelpDescription, const FConsoleCommandDelegate& Delegate)
-			: FAutoConsoleCommand(
-				*BuildFullCommandString(Category, CommandName),
-				*HelpDescription, Delegate,
-				ECVF_Default)
+		FFUAutoConsoleCommand(const FString& Category, const FString& CommandName, const FString& HelpDescription, const FConsoleCommandDelegate& Delegate):
+			FAutoConsoleCommand(*BuildFullCommandString(Category, CommandName), *HelpDescription, Delegate, ECVF_Default)
 		{}
 	};
 	
 	/* No world, with args */
 	struct FFUAutoConsoleCommandWithArgs : private FAutoConsoleCommand
 	{
-		FFUAutoConsoleCommandWithArgs(const FString& Category, const FString& CommandName,
-			const FString& HelpDescription, const FConsoleCommandWithArgsDelegate& Delegate)
-			: FAutoConsoleCommand(
-				*BuildFullCommandString(Category, CommandName),
-				*HelpDescription, Delegate,
-				ECVF_Default)
+		FFUAutoConsoleCommandWithArgs(const FString& Category, const FString& CommandName, const FString& HelpDescription, const FConsoleCommandWithArgsDelegate& Delegate):
+			FAutoConsoleCommand(*BuildFullCommandString(Category, CommandName), *HelpDescription, Delegate, ECVF_Default)
 		{}
 	};
 	
 	/* World, no args */
 	struct FFUAutoConsoleCommandWithWorld : private FAutoConsoleCommandWithWorld
 	{
-		FFUAutoConsoleCommandWithWorld(const FString& Category, const FString& CommandName,
-			const FString& HelpDescription, const FConsoleCommandWithWorldDelegate& Delegate)
-			: FAutoConsoleCommandWithWorld(
-				*BuildFullCommandString(Category, CommandName),
-				*HelpDescription, Delegate,
-				ECVF_Default)
+		FFUAutoConsoleCommandWithWorld(const FString& Category, const FString& CommandName, const FString& HelpDescription, const FConsoleCommandWithWorldDelegate& Delegate):
+			FAutoConsoleCommandWithWorld(*BuildFullCommandString(Category, CommandName), *HelpDescription, Delegate, ECVF_Default)
 		{}
 	};
 	
 	/* World and with args */
 	struct FFUAutoConsoleCommandWithWorldAndArgs : private FAutoConsoleCommandWithWorldAndArgs
 	{
-		FFUAutoConsoleCommandWithWorldAndArgs(const FString& Category, const FString& CommandName,
-			const FString& HelpDescription, const FConsoleCommandWithWorldAndArgsDelegate& Delegate)
-			: FAutoConsoleCommandWithWorldAndArgs(
-				*BuildFullCommandString(Category, CommandName),
-				*HelpDescription, Delegate,
-				ECVF_Default)
+		FFUAutoConsoleCommandWithWorldAndArgs(const FString& Category, const FString& CommandName, const FString& HelpDescription, const FConsoleCommandWithWorldAndArgsDelegate& Delegate):
+			FAutoConsoleCommandWithWorldAndArgs(*BuildFullCommandString(Category, CommandName), *HelpDescription, Delegate, ECVF_Default)
 		{}
 	};
 	
