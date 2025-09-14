2bd6b4028802fac82bcea317c53257f6d5d8235c
hzFishy <84443782+hzFishy@users.noreply.github.com>
2025-09-14 19:33:25 +0200

    added static mesh component

---
+++ Source/FishyUtils/Private/Classes/Engine/FUStaticMeshComponent.cpp
@@ -0,0 +1,10 @@
+﻿// By hzFishy 2025 - Do whatever you want with it
+
+#include "Classes/Engine/FUStaticMeshComponent.h"
+
+
+UFUStaticMeshComponent::UFUStaticMeshComponent(const FObjectInitializer& ObjectInitializer): Super(ObjectInitializer)
+{
+	Mobility = EComponentMobility::Static;
+	SetGenerateOverlapEvents(false);
+}
---
+++ Source/FishyUtils/Public/Classes/Engine/FUStaticMeshComponent.h
@@ -0,0 +1,18 @@
+﻿// By hzFishy 2025 - Do whatever you want with it
+
+#pragma once
+
+#include "FUStaticMeshComponent.generated.h"
+
+
+/**
+ * Fishy Utils base class for static mesh components
+ */
+UCLASS()
+class FISHYUTILS_API UFUStaticMeshComponent : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+
+public:
+	UFUStaticMeshComponent(const FObjectInitializer& ObjectInitializer);
+};
