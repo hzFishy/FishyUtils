2837a4f56c00224b427e6f654b4da41408a837a5
hzFishy <84443782+hzFishy@users.noreply.github.com>
2025-09-14 16:29:05 +0200

    added player camera manager view overrides

---
+++ Source/FishyUtils/Private/Classes/Player/FUPlayerCameraManager.cpp
@@ -0,0 +1,167 @@
+﻿// By hzFishy 2025 - Do whatever you want with it
+
+
+#include "Classes/Player/FUPlayerCameraManager.h"
+
+#include "Asserts/FUAsserts.h"
+
+
+FFUCameraManagerSnapshot::FFUCameraManagerSnapshot():
+	ViewMin(0),
+	ViewMax(0)
+{}
+
+void FFUCameraManagerSnapshot::MakeSnapshot(EFUCameraViewType Type, AFUPlayerCameraManager* PCM)
+{
+	if (!IsValid(PCM)) { return; }
+
+	switch (Type)
+	{
+		case EFUCameraViewType::Pitch:
+			{
+				ViewMin = PCM->ViewPitchMin;
+				ViewMax = PCM->ViewPitchMax;
+				break;
+			}
+		case EFUCameraViewType::Yaw:
+			{
+				ViewMin = PCM->ViewYawMin;
+				ViewMax = PCM->ViewYawMax;
+				break;
+			}
+		case EFUCameraViewType::Roll:
+			{
+				ViewMin = PCM->ViewRollMin;
+				ViewMax = PCM->ViewRollMax;
+				break;
+			}
+		default:
+			break;
+	}
+}
+
+
+FFUCameraViewOverride::FFUCameraViewOverride(): Type(EFUCameraViewType::None)
+{}
+
+FFUCameraViewOverride::FFUCameraViewOverride(EFUCameraViewType InType, float Min, float Max):
+	Type(InType),
+	ViewMin(Min),
+	ViewMax(Max)
+{}
+
+void FFUCameraViewOverride::Apply(AFUPlayerCameraManager* PCM)
+{
+	if (!IsValid(PCM)) { return; }
+	if (Type == EFUCameraViewType::None) { return; }
+
+	if (auto* MinVal = ViewMin.GetPtrOrNull())
+	{
+		ApplyInternal(MinVal, GetPCMViewVarPtr(PCM, true));
+	}
+	
+	if (auto* MaxVal = ViewMax.GetPtrOrNull())
+	{
+		ApplyInternal(MaxVal, GetPCMViewVarPtr(PCM, false));
+	}
+}
+
+void FFUCameraViewOverride::ResetToSnapshot(AFUPlayerCameraManager* PCM)
+{
+	ApplyInternal(&Snapshot.ViewMin, GetPCMViewVarPtr(PCM, true));
+	ApplyInternal(&Snapshot.ViewMax, GetPCMViewVarPtr(PCM, false));
+}
+
+void FFUCameraViewOverride::ApplyInternal(float* Value, float* PCMDest)
+{
+	*PCMDest = *Value;
+}
+
+float* FFUCameraViewOverride::GetPCMViewVarPtr(APlayerCameraManager* PCM, bool bGetMin)
+{
+	switch (Type)
+	{
+		case EFUCameraViewType::Pitch:
+			{
+				return bGetMin ? &PCM->ViewPitchMin : &PCM->ViewPitchMax;
+			}
+		case EFUCameraViewType::Yaw:
+			{
+				return bGetMin ? &PCM->ViewYawMin : &PCM->ViewYawMax;
+			}
+		case EFUCameraViewType::Roll:
+			{
+				return bGetMin ? &PCM->ViewRollMin : &PCM->ViewRollMax;
+			}
+		default:
+			return nullptr;
+	}
+}
+
+FFUCameraViewOverrideEntry::FFUCameraViewOverrideEntry(AFUPlayerCameraManager* PCM, const FFUCameraViewOverride& Override)
+{
+	AddOverride(PCM, Override);
+}
+
+void FFUCameraViewOverrideEntry::AddOverride(AFUPlayerCameraManager* PCM, const FFUCameraViewOverride& Override)
+{
+	auto& NewOverride = Overrides.Emplace(Override.Type, Override);
+	NewOverride.Apply(PCM);
+}
+
+void FFUCameraViewOverrideEntry::RemoveOverride(AFUPlayerCameraManager* PCM, EFUCameraViewType Type)
+{
+	if (auto* Override = Overrides.Find(Type))
+	{
+		Override->ResetToSnapshot(PCM);
+		Overrides.Remove(Type);
+	}
+}
+
+
+FFUCameraViewOverrideContainer::FFUCameraViewOverrideContainer() {}
+
+FFUCameraViewOverrideContainer::FFUCameraViewOverrideContainer(AFUPlayerCameraManager* PCM):
+	OwnerPCM(PCM)
+{}
+
+bool FFUCameraViewOverrideContainer::HasEntry(FName Name) const
+{
+	return ActiveEntries.Contains(Name);
+}
+
+void FFUCameraViewOverrideContainer::AddEntry(FName Name, const FFUCameraViewOverride& Override)
+{
+	auto* MutableOverride = const_cast<FFUCameraViewOverride*>(&Override);
+	MutableOverride->Snapshot.MakeSnapshot(Override.Type, OwnerPCM.Get());
+	
+	if (auto* Entry = ActiveEntries.Find(Name))
+	{
+		Entry->AddOverride(OwnerPCM.Get(), Override);
+	}
+	else if (FU_ENSURE_WEAKVALID_MSG(OwnerPCM, "OwnerPCM is invalid"))
+	{
+		ActiveEntries.Emplace(Name, FFUCameraViewOverrideEntry(OwnerPCM.Get(), Override));
+	}
+}
+
+void FFUCameraViewOverrideContainer::RemoveEntry(FName Name, EFUCameraViewType Type)
+{
+	if (auto* Entry = ActiveEntries.Find(Name))
+	{
+		Entry->RemoveOverride(OwnerPCM.Get(), Type);
+
+		if (Entry->Overrides.IsEmpty())
+		{
+			ActiveEntries.Remove(Name);
+		}
+	}
+}
+
+
+/*----------------------------------------------------------------------------
+		Defaults
+	----------------------------------------------------------------------------*/
+AFUPlayerCameraManager::AFUPlayerCameraManager():
+	ViewOverrideContainer(this)
+{}
---
+++ Source/FishyUtils/Public/Classes/Player/FUPlayerCameraManager.h
@@ -0,0 +1,98 @@
+﻿// By hzFishy 2025 - Do whatever you want with it
+
+#pragma once
+
+#include "FUPlayerCameraManager.generated.h"
+
+
+enum class EFUCameraViewType
+{
+	None,
+	Pitch,
+	Yaw,
+	Roll
+};
+
+struct FISHYUTILS_API FFUCameraManagerSnapshot
+{
+	FFUCameraManagerSnapshot();
+
+	void MakeSnapshot(EFUCameraViewType Type, AFUPlayerCameraManager* PCM);
+	
+	float ViewMin;
+	float ViewMax;
+};
+
+struct FISHYUTILS_API FFUCameraViewOverride
+{
+	FFUCameraViewOverride();
+	FFUCameraViewOverride(EFUCameraViewType InType, float Min, float Max);
+
+	void Apply(AFUPlayerCameraManager* PCM);
+	void ResetToSnapshot(AFUPlayerCameraManager* PCM);
+	
+	// state before being applied
+	FFUCameraManagerSnapshot Snapshot;
+	EFUCameraViewType Type;
+	TOptional<float> ViewMin;
+	TOptional<float> ViewMax;
+
+protected:
+	void ApplyInternal(float* Value, float* PCMDest);
+	float* GetPCMViewVarPtr(APlayerCameraManager* PCM, bool bGetMin);
+};
+
+struct FISHYUTILS_API FFUCameraViewOverrideEntry
+{
+	FFUCameraViewOverrideEntry(AFUPlayerCameraManager* PCM, const FFUCameraViewOverride& Override);
+
+	void AddOverride(AFUPlayerCameraManager* PCM, const FFUCameraViewOverride& Override);
+	void RemoveOverride(AFUPlayerCameraManager* PCM, EFUCameraViewType Type);
+
+	TMap<EFUCameraViewType, FFUCameraViewOverride> Overrides;
+};
+
+struct FISHYUTILS_API FFUCameraViewOverrideContainer
+{
+	FFUCameraViewOverrideContainer();
+	FFUCameraViewOverrideContainer(AFUPlayerCameraManager* PCM);
+	
+	TWeakObjectPtr<AFUPlayerCameraManager> OwnerPCM;
+	
+	bool HasEntry(FName Name) const;
+
+	/** Writes a snapshot of the PCM and applies the override */
+	void AddEntry(FName Name, const FFUCameraViewOverride& Override);
+
+	/** If found removed the entry of the given type and applies snapshot values */
+	void RemoveEntry(FName Name, EFUCameraViewType Type);
+	
+protected:
+	TMap<FName, FFUCameraViewOverrideEntry> ActiveEntries;
+};
+
+
+/**
+ * 
+ */
+UCLASS(DisplayName="Fishy Player Camera Manager")
+class FISHYUTILS_API AFUPlayerCameraManager : public APlayerCameraManager
+{
+	GENERATED_BODY()
+
+	
+	/*----------------------------------------------------------------------------
+		Properties
+	----------------------------------------------------------------------------*/
+public:
+	FFUCameraViewOverrideContainer ViewOverrideContainer;
+	
+	
+	/*----------------------------------------------------------------------------
+		Defaults
+	----------------------------------------------------------------------------*/
+public:
+	AFUPlayerCameraManager();
+
+	
+};
