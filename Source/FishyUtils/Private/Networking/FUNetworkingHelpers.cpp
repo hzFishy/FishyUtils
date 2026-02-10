ed45dc0ba5ada9fd0ef05d91d4e96961ee3016f1
hzFishy <84443782+hzFishy@users.noreply.github.com>
2026-02-10 03:14:52 +0100

    Added NET push helpers

---
+++ Source/FishyUtils/Private/Networking/FUNetworkingHelpers.cpp
@@ -0,0 +1,4 @@
+﻿// By hzFishy - 2026 - Do whatever you want with it.
+
+
+#include "Networking/FUNetworkingHelpers.h"
---
+++ Source/FishyUtils/Public/Networking/FUNetworkingHelpers.h
@@ -0,0 +1,14 @@
+﻿// By hzFishy - 2026 - Do whatever you want with it.
+#pragma once
+
+#include "Net/Core/PushModel/PushModel.h"
+
+
+/** For push model */
+#define FU_NET_MARKDIRTY(PropertyName) MARK_PROPERTY_DIRTY_FROM_NAME(ThisClass, PropertyName, this)
+
+
+/** Call dirty for push model and call OnRep func (usually for listen servers) */
+#define FU_NET_MARKDIRTY_ONREPCALL(PropertyName) \
+	FU_NET_MARKDIRTY(PropertyName) \
+	OnRep_##PropertyName() \
