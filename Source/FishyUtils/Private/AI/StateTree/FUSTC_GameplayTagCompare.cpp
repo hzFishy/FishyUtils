0c65e8ed164ecd7ced90315d320b68cd755009f6
hzFishy <84443782+hzFishy@users.noreply.github.com>
2026-02-15 20:22:40 +0100

    Added State Tree Condition compare for gameplaytags

---
+++ Source/FishyUtils/Private/AI/StateTree/FUSTC_GameplayTagCompare.cpp
@@ -0,0 +1,43 @@
+﻿// Copyright Prison Mars Dev Team. All Rights Reserved.
+
+
+#include "AI/StateTree/FUSTC_GameplayTagCompare.h"
+#include "StateTreeExecutionContext.h"
+
+
+FFUSTC_GameplayTagCompare::FFUSTC_GameplayTagCompare(): 
+	bExactMatch(false), 
+	bInvert(false)
+{}
+
+bool FFUSTC_GameplayTagCompare::TestCondition(FStateTreeExecutionContext& Context) const
+{
+	const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+	
+	bool bResult;
+	
+	if (bExactMatch)
+	{
+		bResult = InstanceData.TagA.MatchesTagExact(InstanceData.TagB);
+	}
+	else
+	{
+		bResult = InstanceData.TagA.MatchesTag(InstanceData.TagB);
+	}
+	
+	return bInvert ? !bResult : bResult;
+}
+
+#if WITH_EDITOR
+FText FFUSTC_GameplayTagCompare::GetDescription(const FGuid& ID, FStateTreeDataView InstanceDataView, const IStateTreeBindingLookup& BindingLookup, EStateTreeNodeFormatting Formatting) const
+{
+	const FInstanceDataType& InstanceData = InstanceDataView.Get<FInstanceDataType>();
+	
+	const FString Result = FString::Printf(TEXT("GameplayTag Compare (%s) (%s) %s"),
+		*InstanceData.TagB.ToString(),
+		*FString(bExactMatch ? "Exact" : "NotExact"),
+		*FString(bInvert ? "(Inverted)" : "")
+	);
+	return FText::FromString(Result);
+}
+#endif
---
+++ Source/FishyUtils/Public/AI/StateTree/FUSTC_GameplayTagCompare.h
@@ -0,0 +1,45 @@
+﻿// Copyright Prison Mars Dev Team. All Rights Reserved.
+
+#pragma once
+
+#include "StateTreeConditionBase.h"
+#include "FUSTC_GameplayTagCompare.generated.h"
+
+USTRUCT()
+struct FFUSTC_GameplayTagCompareInstanceData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	FGameplayTag TagA;
+
+	UPROPERTY(EditAnywhere, Category = "Parameter")
+	FGameplayTag TagB;
+};
+
+/**
+ * Condition comparing two gameplay tags.
+ */
+USTRUCT(DisplayName="GameplayTag Compare", Category="FishyUtils")
+struct FFUSTC_GameplayTagCompare : public FStateTreeConditionCommonBase
+{
+	GENERATED_BODY()
+
+	using FInstanceDataType = FFUSTC_GameplayTagCompareInstanceData;
+
+	FFUSTC_GameplayTagCompare();
+	
+	virtual const UStruct* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
+	
+	virtual bool TestCondition(FStateTreeExecutionContext& Context) const override;
+	
+#if WITH_EDITOR
+	virtual FText GetDescription(const FGuid& ID, FStateTreeDataView InstanceDataView, const IStateTreeBindingLookup& BindingLookup, EStateTreeNodeFormatting Formatting = EStateTreeNodeFormatting::Text) const override;
+#endif
+	
+	UPROPERTY(EditAnywhere, Category="Parameter")
+	bool bExactMatch;
+	
+	UPROPERTY(EditAnywhere, Category="Parameter")
+	bool bInvert;	
+};
