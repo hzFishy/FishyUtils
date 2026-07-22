5665db7839448c8660c4e3f324ab5fcdfd0f363d
hzFishy <84443782+hzFishy@users.noreply.github.com>
2026-07-22 14:06:23 +0200

    Added ExtraTick WorldSubsystem

---
+++ Source/FishyUtils/Private/Utility/FUExtraTickWorldSubsystem.cpp
@@ -0,0 +1,159 @@
+﻿// By hzFishy - 2026 - Do whatever you want with it.
+
+
+#include "Utility/FUExtraTickWorldSubsystem.h"
+
+	
+	/*----------------------------------------------------------------------------
+		Defaults
+	----------------------------------------------------------------------------*/
+FFUExtraTickFunction::FFUExtraTickFunction(): 
+	Target(nullptr), 
+	Group(TG_MAX)
+{
+	// TODO: make this customizable per registrant
+	
+	bCanEverTick = true;
+	bHighPriority = false;
+	bRunOnAnyThread = false;
+	bTickEvenWhenPaused = false;
+	TickInterval = 0.0f;
+}
+
+void FFUExtraTickFunction::ExecuteTick(float DeltaTime, ELevelTick TickType, ENamedThreads::Type CurrentThread, const FGraphEventRef& MyCompletionEventGraph)
+{
+	if (IsValid(Target) && TickType != LEVELTICK_ViewportsOnly)
+	{
+		Target->HandleTick(DeltaTime, TickGroup);
+	}
+}
+
+FString FFUExtraTickFunction::DiagnosticMessage()
+{
+	return "FFUExtraTickFunction";
+}
+
+	
+	/*----------------------------------------------------------------------------
+		Defaults
+	----------------------------------------------------------------------------*/
+UFUExtraTickWorldSubsystem::UFUExtraTickWorldSubsystem()
+{}
+
+void UFUExtraTickWorldSubsystem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+	
+	PrePhysics.Target = this;
+    PrePhysics.TickGroup = ETickingGroup::TG_PrePhysics;
+	PrePhysics.RegisterTickFunction(GetWorld()->PersistentLevel.Get());
+	
+    StartPhysics.Target = this;
+    StartPhysics.TickGroup = ETickingGroup::TG_StartPhysics;
+	StartPhysics.RegisterTickFunction(GetWorld()->PersistentLevel.Get());
+	
+    DuringPhysics.Target = this;
+    DuringPhysics.TickGroup = ETickingGroup::TG_DuringPhysics;
+	DuringPhysics.RegisterTickFunction(GetWorld()->PersistentLevel.Get());
+	
+    EndPhysics.Target = this;
+    EndPhysics.TickGroup = ETickingGroup::TG_EndPhysics;
+	EndPhysics.RegisterTickFunction(GetWorld()->PersistentLevel.Get());
+	
+    PostPhysics.Target = this;
+    PostPhysics.TickGroup = ETickingGroup::TG_PostPhysics;
+	PostPhysics.RegisterTickFunction(GetWorld()->PersistentLevel.Get());
+	
+    PostUpdateWork.Target = this;
+    PostUpdateWork.TickGroup = ETickingGroup::TG_PostUpdateWork;
+	PostUpdateWork.RegisterTickFunction(GetWorld()->PersistentLevel.Get());
+	
+    LastDemotable.Target = this;
+    LastDemotable.TickGroup = ETickingGroup::TG_LastDemotable;
+	LastDemotable.RegisterTickFunction(GetWorld()->PersistentLevel.Get());
+}
+
+void UFUExtraTickWorldSubsystem::Deinitialize()
+{
+	Super::Deinitialize();
+	
+	PrePhysics.UnRegisterTickFunction();
+	
+	StartPhysics.UnRegisterTickFunction();
+	
+	DuringPhysics.UnRegisterTickFunction();
+	
+	EndPhysics.UnRegisterTickFunction();
+	
+	PostPhysics.UnRegisterTickFunction();
+	
+	PostUpdateWork.UnRegisterTickFunction();
+	
+	LastDemotable.UnRegisterTickFunction();
+}
+
+	
+	/*----------------------------------------------------------------------------
+		Core
+	----------------------------------------------------------------------------*/
+void UFUExtraTickWorldSubsystem::K2_RegisterCallback(FFUExtraTickDynamicDelegate NewRegistrant, EFUExtraTickGroup TickGroup)
+{
+	GetTicker(GetEngineTickGroup(TickGroup)).DynamicDelegate.Add(NewRegistrant);
+}
+
+void UFUExtraTickWorldSubsystem::RegisterCallback(FFUExtraTickDelegate NewRegistrant, EFUExtraTickGroup TickGroup)
+{
+	GetTicker(GetEngineTickGroup(TickGroup)).NativeDelegate.Add(NewRegistrant);
+}
+
+void UFUExtraTickWorldSubsystem::HandleTick(float DeltaTime, ETickingGroup Group)
+{
+	FFUExtraTickFunction& Ticker = GetTicker(Group);
+	Ticker.NativeDelegate.Broadcast(DeltaTime);
+	Ticker.DynamicDelegate.Broadcast(DeltaTime);
+}
+
+ETickingGroup UFUExtraTickWorldSubsystem::GetEngineTickGroup(EFUExtraTickGroup Group)
+{
+	return static_cast<ETickingGroup>(Group);
+}
+
+FFUExtraTickFunction& UFUExtraTickWorldSubsystem::GetTicker(ETickingGroup TickGroup)
+{
+	switch (TickGroup)
+	{
+	case TG_PrePhysics:
+		{
+			return PrePhysics;
+		}
+	case TG_StartPhysics:
+		{
+			return StartPhysics;
+		}
+	case TG_DuringPhysics:
+		{
+			return DuringPhysics;
+		}
+	case TG_EndPhysics:
+		{
+			return EndPhysics;
+		}
+	case TG_PostPhysics:
+		{
+			return PostPhysics;
+		}
+	case TG_PostUpdateWork:
+		{
+			return PostUpdateWork;
+		}
+	case TG_LastDemotable:
+		{
+			return LastDemotable;
+		}
+	default:
+		{
+			checkNoEntry();
+			return PrePhysics;
+		}
+	}
+}
---
+++ Source/FishyUtils/Public/Utility/FUExtraTickWorldSubsystem.h
@@ -0,0 +1,138 @@
+﻿// By hzFishy - 2026 - Do whatever you want with it.
+// Thanks to https://github.com/HomerJohnston/TickerBee for the initial code
+
+#pragma once
+
+#include "Subsystems/WorldSubsystem.h"
+#include "FUExtraTickWorldSubsystem.generated.h"
+
+
+DECLARE_DYNAMIC_DELEGATE_OneParam(FFUExtraTickDynamicDelegate, float, DeltaTime);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FFUExtraTickDynamicMultiDelegate, float, DeltaTime);
+
+using FFUExtraTickDelegate = TDelegate<void(float)>;
+
+UENUM(BlueprintType, DisplayName="Extra Tick Group")
+enum class EFUExtraTickGroup : uint8
+{
+	/** Any item that needs to be executed before physics simulation starts. */
+	PrePhysics UMETA(DisplayName="Pre Physics"),
+	
+	/** Special tick group that starts physics simulation. */							
+	StartPhysics UMETA(DisplayName="Start Physics"),
+	
+	/** Any item that can be run in parallel with our physics simulation work. */
+	DuringPhysics UMETA(DisplayName="During Physics"),
+	
+	/** Special tick group that ends physics simulation. */
+	EndPhysics UMETA(DisplayName="End Physics"),
+	
+	/** Any item that needs rigid body and cloth simulation to be complete before being executed. */
+	PostPhysics UMETA(DisplayName="Post Physics"),
+	
+	/** Any item that needs the update work to be done before being ticked. */
+	PostUpdateWork UMETA(DisplayName="Post Update Work"),
+	
+	/** Catchall for anything demoted to the end. */
+	LastDemotable UMETA(DisplayName = "Last Demotable"),
+};
+
+
+USTRUCT()
+struct FFUExtraTickFunction : public FTickFunction
+{
+	GENERATED_BODY()
+	
+	
+	/*----------------------------------------------------------------------------
+		Properties
+	----------------------------------------------------------------------------*/
+	UPROPERTY(Transient)
+	UFUExtraTickWorldSubsystem* Target;
+	
+	ETickingGroup Group;
+	
+	UPROPERTY(Transient)
+	FFUExtraTickDynamicMultiDelegate DynamicDelegate;
+	
+	TMulticastDelegate<void(float)> NativeDelegate;
+    
+	
+	/*----------------------------------------------------------------------------
+		Defaults
+	----------------------------------------------------------------------------*/
+public:
+	FFUExtraTickFunction();
+
+	virtual void ExecuteTick(float DeltaTime, ELevelTick TickType, ENamedThreads::Type CurrentThread, const FGraphEventRef& MyCompletionEventGraph) override;
+
+	virtual FString DiagnosticMessage() override;
+};
+
+template<>
+struct TStructOpsTypeTraits<FFUExtraTickFunction> : public TStructOpsTypeTraitsBase2<FFUExtraTickFunction>
+{
+	enum
+	{
+		WithCopy = false
+	};
+};
+
+
+UCLASS(DisplayName="Extra Tick World Subsystem")
+class FISHYUTILS_API UFUExtraTickWorldSubsystem : public UWorldSubsystem
+{
+	GENERATED_BODY()
+	
+	friend FFUExtraTickFunction;
+	
+	
+	/*----------------------------------------------------------------------------
+		Properties
+	----------------------------------------------------------------------------*/
+protected:
+	FFUExtraTickFunction PrePhysics;
+	
+	FFUExtraTickFunction StartPhysics;
+	
+	FFUExtraTickFunction DuringPhysics;
+	
+	FFUExtraTickFunction EndPhysics;
+	
+	FFUExtraTickFunction PostPhysics;
+	
+	FFUExtraTickFunction PostUpdateWork;
+	
+	FFUExtraTickFunction LastDemotable;
+	
+	
+	/*----------------------------------------------------------------------------
+		Defaults
+	----------------------------------------------------------------------------*/
+public:
+	UFUExtraTickWorldSubsystem();
+	
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	
+	virtual void Deinitialize() override;
+	
+	
+	/*----------------------------------------------------------------------------
+		Core
+	----------------------------------------------------------------------------*/
+public:
+	UFUNCTION(BlueprintCallable, DisplayName="Register Callback", Category="FishyUtils|ExtraTick")
+	void K2_RegisterCallback(FFUExtraTickDynamicDelegate NewRegistrant, EFUExtraTickGroup TickGroup);
+
+	/** 
+	 *  Warning: This will crash if the subsystem wasn't initialized.
+	 */
+	void RegisterCallback(FFUExtraTickDelegate NewRegistrant, EFUExtraTickGroup TickGroup);
+
+protected:
+	void HandleTick(float DeltaTime, ETickingGroup Group);
+	
+	ETickingGroup GetEngineTickGroup(EFUExtraTickGroup Group);
+	
+	FFUExtraTickFunction& GetTicker(ETickingGroup TickGroup);
+};
